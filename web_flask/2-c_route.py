eb78f1daee65dd67b6c7c2098d256d64f431bdf9
root <knashipa@gmail.com>
2022-01-29 16:54:50 +0000

    C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -7,18 +7,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     """ Display the site index """
     return "Hello HBNB!"
+
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-return "HBNB"
+    return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
-return "C " + text.replace("_", " ")
+    return "C " + text.replace("_", " ")
 
 if __name__ == '__main__':
-app.run(debug=True, host='0.0.0.0', port=500)
- 
+    app.run(debug=True, host='0.0.0.0', port=5000)
